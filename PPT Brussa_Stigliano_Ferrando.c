bcce839fffc361d25b5cc1557f11b811cf73937c
EnzoStigliano <131629043+EnzoStigliano@users.noreply.github.com>
2024-04-29 16:20:20 -0300

    El mismo archivo pero ahora con comentarios

--- PPT Brussa_Stigliano_Ferrando.c
+++ PPT Brussa_Stigliano_Ferrando.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 int main(void){
+	//Declaración de variables
 	int 
 		jugada,
 		maquina,
@@ -13,18 +14,22 @@ int main(void){
 		i,
 		j;
 	
+	//For para las rondas
 	for(i=0;i<5;i++){
 		printf("Ronda %d\n\n", i+1);
 		acumJugador = 0;
 		acumMaquina = 0;
 
+		//For para las vueltas
 		for(j=0;j<3;j++){
 			printf("Ingrese pieda (1), papel (2) o tijera (3): ");
 			scanf("%d", &jugada);
 	
+			//Establecer rangos para los números aleatorios
 			srand(time(NULL));
 			maquina = 1 + rand()%(4 - 1);
 
+			//Indicar victoria, derrota o empate dependiendo del caso
 			if(jugada == 1 && maquina == 1){
 				printf("Piedra - Pieda: Empate");
 			}
@@ -63,12 +68,15 @@ int main(void){
 			printf("\n\n");
 		}
 
+		
+		//Acumulador de puntos
 		if(acumJugador > acumMaquina){
 			acumPuntosJ++;
 		}
 		else acumPuntosM++;
 	}
 	
+	//Resultado
 	printf("%d - %d", acumPuntosJ, acumPuntosM);
 	if(acumPuntosJ > acumPuntosM){
 		printf("\nGanaste!");
