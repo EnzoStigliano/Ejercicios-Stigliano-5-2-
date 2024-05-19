9e8068f6a7da36babc44ee14b8c55803984a0674
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2024-05-19 12:37:20 -0300

    agregado el menu (de nuevo) y las 2 cargas del juego

--- Pirata.c
+++ Pirata.c
@@ -14,7 +14,8 @@ int inicializarTablero(int **tablero, int f, int c, int posicionPirata[2]) {
   srand(time(NULL));
   int salidas[f][c]; // Matriz donde se guardaran las salidas
   int dungeons[16][4] = {
-      // Lista de posibles salidas, si tiene un 1 es porque tiene salida para ese lado
+      // Lista de posibles salidas, si tiene un 1 es porque tiene salida para
+      // ese lado
       // Orden: izquierda, derecha, arriba, abajo
       {0, 0, 0, 0}, {0, 0, 0, 1}, {0, 0, 1, 0}, {0, 0, 1, 1},
       {0, 1, 0, 0}, {0, 1, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 1},
@@ -62,7 +63,7 @@ int inicializarTablero(int **tablero, int f, int c, int posicionPirata[2]) {
           // Cada vez que se genere un bloque de tierra se contara la cantidad
           // de estos generados y si efectivamente se ha generado alguno
 
-          // Generar un bloque de tierra si hay otro a la izquierda 
+          // Generar un bloque de tierra si hay otro a la izquierda
           // y si ademas este tiene salida hacia la derecha
           if (j - 1 >= 0 && salidas[i][j - 1] != 16) {
             if (dungeons[salidas[i][j - 1]][0] == 1) {
@@ -71,8 +72,9 @@ int inicializarTablero(int **tablero, int f, int c, int posicionPirata[2]) {
               cantDungeons++;
               flag = 1;
             }
-          } else if (i - 1 >= 0 && salidas[i - 1][j] != 16) 
-          { // Generar un bloque de tierra si hay otro arriba
+          } else if (i - 1 >= 0 &&
+                     salidas[i - 1][j] !=
+                         16) { // Generar un bloque de tierra si hay otro arriba
             // y si ademas este tiene salida hacia abajo
             if (dungeons[salidas[i - 1][j]][2] == 1) {
               salida = rand() % 15;
@@ -80,8 +82,8 @@ int inicializarTablero(int **tablero, int f, int c, int posicionPirata[2]) {
               cantDungeons++;
               flag = 1;
             }
-          } else if (j + 1 < c && salidas[i][j + 1] != 16) { 
-            // Generar un bloque de tierra si hay otro a la derecha 
+          } else if (j + 1 < c && salidas[i][j + 1] != 16) {
+            // Generar un bloque de tierra si hay otro a la derecha
             // y si ademas este tiene salida hacia la izquierda
             if (dungeons[salidas[i][j + 1]][1] == 1) {
               salida = rand() % 15;
@@ -89,8 +91,7 @@ int inicializarTablero(int **tablero, int f, int c, int posicionPirata[2]) {
               cantDungeons++;
               flag = 1;
             }
-          } else if (i + 1 < f &&
-                     salidas[i + 1][j] != 16) { 
+          } else if (i + 1 < f && salidas[i + 1][j] != 16) {
             // Generar un bloque de tierra si hay otro abajo
             // y si ademas este tiene salida hacia arriba
             if (dungeons[salidas[i + 1][j]][3] == 1) {
@@ -162,7 +163,7 @@ void dibujarTablero(int **tablero, int f, int c) {
 
       case 20:
         printf("\033[0;32m██\033[0m"); // Color verde para tierra con tesoro
-        break
+        break;
 
       case 21:
         printf("\033[0;31m██\033[0m"); // Color rojo para pirata
@@ -182,7 +183,9 @@ int moverPirata(int posicionPirata[2], char mov, int **tablero, int f, int c,
 
   switch (mov) {
   case 'n':
-    if (posicionPirata[0] - 1 >= 0 && tablero[posicionPirata[0] - 1][posicionPirata[1]] != 1) // Fijarse si arriba no hay agua
+    if (posicionPirata[0] - 1 >= 0 &&
+        tablero[posicionPirata[0] - 1][posicionPirata[1]] !=
+            1) // Fijarse si arriba no hay agua
     {
       posicionPirata[0]--;
     } else {
@@ -192,7 +195,9 @@ int moverPirata(int posicionPirata[2], char mov, int **tablero, int f, int c,
     break;
 
   case 's':
-    if (posicionPirata[0] + 1 < f && tablero[posicionPirata[0]+1][posicionPirata[1]] != 1) // Fijarse si abajo no hay agua
+    if (posicionPirata[0] + 1 < f &&
+        tablero[posicionPirata[0] + 1][posicionPirata[1]] !=
+            1) // Fijarse si abajo no hay agua
     {
       posicionPirata[0]++;
     } else {
@@ -202,7 +207,9 @@ int moverPirata(int posicionPirata[2], char mov, int **tablero, int f, int c,
     break;
 
   case 'e':
-    if (posicionPirata[1] + 1 < c && tablero[posicionPirata[0]][posicionPirata[1]+1] != 1) // Fijarse si a la derecha no hay agua
+    if (posicionPirata[1] + 1 < c &&
+        tablero[posicionPirata[0]][posicionPirata[1] + 1] !=
+            1) // Fijarse si a la derecha no hay agua
     {
       posicionPirata[1]++;
     } else {
@@ -212,7 +219,9 @@ int moverPirata(int posicionPirata[2], char mov, int **tablero, int f, int c,
     break;
 
   case 'o':
-    if (posicionPirata[1] - 1 >= 0 && tablero[posicionPirata[0]][posicionPirata[1]-1] != 1) // Fijarse si a la izquierda no hay agua
+    if (posicionPirata[1] - 1 >= 0 &&
+        tablero[posicionPirata[0]][posicionPirata[1] - 1] !=
+            1) // Fijarse si a la izquierda no hay agua
     {
       posicionPirata[1]--;
     } else {
@@ -235,70 +244,182 @@ int moverPirata(int posicionPirata[2], char mov, int **tablero, int f, int c,
 int main(void) {
   int filas;
   int columnas;
-  int encontrado;        // Variable para verificar si se encontrÃ³ el tesoro
+  int encontrado;        // Variable para verificar si se encontro el tesoro
   int posicionPirata[2]; // Coordenadas del pirata en el tablero
   char mov;              // Direccion de movimiento ingresada por el usuario
-  int turnos;            // Numero mÃ¡ximo de turnos
+  int turnos;            // Numero maximo de turnos
   int salir = 0; // Variable para controlar si el usuario quiere salir del juego
+  int opcion = 0;
+  char tecla;
 
-  printf("Busqueda del tesoro! Intente encontrar el tesoro en menos de 50 "
-         "turnos\n\n");
+  while (1) {
+    // Limpiar la pantalla
+    system("clear");
 
-  // Bucle principal del juego
-  do {
-    // Solicitar dimensiones del tablero al usuario
-    do {
-      printf("Ingrese la cantidad de filas del tablero (minimo 4): ");
-      scanf("%d", &filas);
-      printf("Ingrese la cantidad de columnas del tablero (minimo 4): ");
-      scanf("%d", &columnas);
-      if (filas < 4) {
-        printf("La cantidad de filas no cumple con el minimo requerido\n\n");
+    // Dibujar el título
+    printf("\033[1;34m"); // Texto en azul
+    printf("┌─────────────────────────────────┐\n");
+    printf("│\033[1m ¡Bienvenido al juego del tesoro!│ \n"); // Texto mas grande
+    printf("└─────────────────────────────────┘\n");
+    printf("\033[0m"); // Resetear el color
+    printf("\n");
+
+    // Dibujar las opciones
+    printf("\033[1;33m"); // Texto en amarillo
+    printf("  Menu:\n");
+    printf("  1. Jugar\n");
+    printf("  2. Reglas\n");
+    printf("  3. Controles\n");
+    printf("  4. Salir\n");
+    printf("\033[0m"); // Resetear el color
+
+    // Esperar input del usuario
+    printf("\033[1;32m"); // Texto en verde
+    printf("\nIngresa el número de la opcion: ");
+    printf("\033[0m"); // Resetear el color
+    scanf("%d", &opcion);
+
+    switch (opcion) {
+    case 1:
+      // Mostrar juego
+      system("clear");
+
+      for (int carga = 0; carga <= 100; carga += 1) {
+        printf("┌─────────────────────────────────┐\n");
+        printf("│             Cargando...         │\n");
+        printf("│             %3d%%                │\n", carga);
+        printf("└─────────────────────────────────┘\n");
+        usleep(100000); // Pausa de 0.1 segundos
+        system("clear");
       }
-      if (columnas < 4) {
-        printf("La cantidad de columnas no cumple con el minimo requerido\n\n");
-      }
-    } while (filas < 4 || columnas < 4); // Seguir preguntando si las filas o las columnas no cumplen con el minimo
 
-    // Reservacion de memoria para el puntero del tablero
-    int **tablero = (int **)malloc(filas * sizeof(int *));
-    for (int i = 0; i < filas; i++) {
-      tablero[i] = (int *)malloc(columnas * sizeof(int));
+      // Bucle principal del juego
+      do {
+        // Solicitar dimensiones del tablero al usuario
+        do {
+          printf("Ingrese la cantidad de filas del tablero (minimo 4):");
+          scanf("%d", &filas);
+          printf("Ingrese la cantidad de columnas del tablero (minimo 4):");
+          scanf("%d", &columnas);
+          if (filas < 4) {
+            printf(
+                "La cantidad de filas no cumple con el minimo requerido\n\n");
+          }
+          if (columnas < 4) {
+            printf("La cantidad de columnas no cumple con el minimo "
+                   "requerido\n\n");
+          }
+        } while (filas < 4 ||
+                 columnas < 4); // Seguir preguntando si las filas o las
+                                // columnas no cumplen con el minimo
+
+        // Reservacion de memoria para el puntero del tablero
+        int **tablero = (int **)malloc(filas * sizeof(int *));
+        for (int i = 0; i < filas; i++) {
+          tablero[i] = (int *)malloc(columnas * sizeof(int));
+        }
+        limpiarConsola();
+        inicializarTablero(tablero, filas, columnas,
+                           posicionPirata); // Inicializar el tablero
+
+        for (int carga = 0; carga <= 100; carga += 5) {
+          printf("┌─────────────────────────────────┐\n");
+          printf("│          Cargando mapa...       │\n");
+          printf("│             %3d%%                │\n", carga);
+          printf("└─────────────────────────────────┘\n");
+          usleep(200000); // Pausa de 0.2 segundos
+          system("clear");
+        }
+
+        dibujarTablero(tablero, filas, columnas); // Dibujar el tablero
+        encontrado = 0; // Inicializar la variable de tesoro encontrado
+        turnos = 50;
+
+        // Bucle de turnos del juego
+        while (encontrado != 1 && turnos > 0) {
+          printf("Turnos restantes: %d\n", turnos);
+          printf("Ingrese la direccion a la que se quiere mover: ");
+          scanf(" %c", &mov);
+          limpiarConsola(); // Limpiar la consola para mostrar el nuevo estado
+                            // del tablero
+          moverPirata(posicionPirata, mov, tablero, filas, columnas,
+                      &encontrado); // Mover al pirata
+          dibujarTablero(tablero, filas,
+                         columnas); // Dibujar el tablero actualizado
+          if (encontrado == 1) {
+            printf("Encontraste el tesoro!\n");
+          }
+          turnos--;
+        }
+
+        if (turnos == 0 && encontrado == 0) {
+          printf("Se te acabaron los turnos, perdiste!\n");
+        }
+
+        // Liberar la memoria utilizada
+        for (int i = 0; i < filas; i++) {
+          free(tablero[i]);
+        }
+        free(tablero);
+
+        // Preguntar al usuario si desea jugar nuevamente
+        printf("Queres jugar otra vez? (1 = si; 0 = no): ");
+        scanf("%d", &salir);
+      } while (salir == 1); // Repetir el juego si el usuario elige continuar
+      printf("Presiona Enter para volver al menu...\n");
+      getchar();
+
+      break;
+    case 2:
+      // Mostrar reglas
+      system("clear");
+      printf("\033[1;34m"); // Texto en azul
+      printf("┌────────────────────────────────┐\n");
+      printf("│    Reglas del juego:           │\n");
+      printf("├────────────────────────────────┤\n");
+      printf("│- Tienes 50 intentos para       │\n");
+      printf("│  intentar encontrar el tesoro  │\n");
+      printf("│- Evita caer al agua.           │\n");
+      printf("└────────────────────────────────┘\n");
+      printf("\033[0m"); // Resetear el color
+      printf("Presiona Enter para salir.\n");
+      getchar();
+      break;
+    case 3:
+      // Mostrar controles
+      system("clear");
+      printf("\033[1;34m"); // Texto en azul
+      printf("┌────────────────────────────────┐\n");
+      printf("│       Controles del juego:     │\n");
+      printf("├────────────────────────────────┤\n");
+      printf("│- Tecla n (Norte↑):             │\n");
+      printf("│  Desplazamiento hacia arriba.  │\n");
+      printf("│- Tecla s (Sur↓):               │\n");
+      printf("│  Desplazamiento hacia abajo.   │\n");
+      printf("│- Tecla o (Oeste←):             │\n");
+      printf("│  Desplazamiento hacia la       │\n");
+      printf("│  izquierda.                    │\n");
+      printf("│- Tecla e (Este→):              │\n");
+      printf("│  Desplazamiento hacia la       │\n");
+      printf("│  derecha.                      │\n");
+      printf("└────────────────────────────────┘\n");
+      printf("\033[0m"); // Resetear el color
+      printf("Presiona Enter para salir.\n");
+      getchar();
+      break;
+    case 4:
+      // salir
+      return 0;
+    default:
+      printf("\033[1;31m"); // Texto en rojo
+      printf("Opción inválida. Intenta de nuevo.\n");
+      printf("\033[0m"); // Resetear el color
+      getchar();
+      break;
     }
 
-    inicializarTablero(tablero, filas, columnas, posicionPirata); // Inicializar el tablero
-    dibujarTablero(tablero, filas, columnas); // Dibujar el tablero
-    encontrado = 0; // Inicializar la variable de tesoro encontrado
-    turnos = 50;
-
-    // Bucle de turnos del juego
-    while (encontrado != 1 && turnos > 0) {
-      printf("Turnos restantes: %d\n", turnos);
-      printf("Ingrese la direccion a la que se quiere mover: ");
-      scanf(" %c", &mov);
-      limpiarConsola(); // Limpiar la consola para mostrar el nuevo estado del tablero
-      moverPirata(posicionPirata, mov, tablero, filas, columnas, &encontrado); // Mover al pirata
-      dibujarTablero(tablero, filas, columnas); // Dibujar el tablero actualizado
-      if (encontrado == 1) {
-        printf("Encontraste el tesoro!\n");
-      }
-      turnos--;
-    }
-
-    if (turnos == 0 && encontrado == 0) {
-      printf("Se te acabaron los turnos, perdiste!\n");
-    }
-
-    // Liberar la memoria utilizada
-    for (int i = 0; i < filas; i++) {
-      free(tablero[i]);
-    }
-    free(tablero);
-
-    // Preguntar al usuario si desea jugar nuevamente
-    printf("Queres jugar otra vez? (1 = si; 0 = no): ");
-    scanf("%d", &salir);
-  } while (salir == 1); // Repetir el juego si el usuario elige continuar
-
-  return 0;
-} 
+    // Limpiar el buffer de entrada
+    while (getchar() != '\n')
+      ;
+  }
+}
